4365d3d1aa8ddecbf406b967362b338128aad781
acidmanifesto <joshua.lee.betts@gmail.com>
2022-02-07 16:10:51 -0500

    Climb Hack now actually works and reports.

--- src/AnticheatMgr.cpp
+++ src/AnticheatMgr.cpp
@@ -221,8 +221,7 @@ void AnticheatMgr::ClimbHackDetection(Player *player, MovementInfo movementInfo,
 		player->IsFalling())
 		return;
 
-	Position playerPos;
-	// Position pos = player->GetPosition();
+	Position pos = player->GetPosition();
 
 	float deltaZ = fabs(playerPos.GetPositionZ() - movementInfo.pos.GetPositionZ());
 	float deltaXY = movementInfo.pos.GetExactDist2d(&playerPos);
