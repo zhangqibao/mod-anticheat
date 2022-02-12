8cac48ffcad3fb2ff18d04d8e03a313155bf144c
MDIC <joshua.lee.betts@gmail.com>
2022-02-12 12:58:45 -0500

    Update charactersdb_anticheat.sql
    Update to Prevent Depreciation warnings.

--- sql/characters/base/charactersdb_anticheat.sql
+++ sql/characters/base/charactersdb_anticheat.sql
@@ -1,31 +1,31 @@
 DROP TABLE IF EXISTS `daily_players_reports`;
 CREATE TABLE IF NOT EXISTS `daily_players_reports` (
-  `guid` int(10) unsigned NOT NULL DEFAULT '0',
-  `creation_time` int(10) unsigned NOT NULL DEFAULT '0',
-  `average` float NOT NULL DEFAULT '0',
-  `total_reports` bigint(20) unsigned NOT NULL DEFAULT '0',
-  `speed_reports` bigint(20) unsigned NOT NULL DEFAULT '0',
-  `fly_reports` bigint(20) unsigned NOT NULL DEFAULT '0',
-  `jump_reports` bigint(20) unsigned NOT NULL DEFAULT '0',
-  `waterwalk_reports` bigint(20) unsigned NOT NULL DEFAULT '0',
-  `teleportplane_reports` bigint(20) unsigned NOT NULL DEFAULT '0',
-  `climb_reports` bigint(20) unsigned NOT NULL DEFAULT '0',
-  `teleporthack_reports` bigint(20) unsigned NOT NULL DEFAULT '0',
+  `guid` int unsigned NOT NULL DEFAULT 0,
+  `creation_time` int unsigned NOT NULL DEFAULT 0,
+  `average` float NOT NULL DEFAULT 0,
+  `total_reports` bigint unsigned NOT NULL DEFAULT 0,
+  `speed_reports` bigint unsigned NOT NULL DEFAULT 0,
+  `fly_reports` bigint unsigned NOT NULL DEFAULT 0,
+  `jump_reports` bigint unsigned NOT NULL DEFAULT 0,
+  `waterwalk_reports` bigint unsigned NOT NULL DEFAULT 0,
+  `teleportplane_reports` bigint unsigned NOT NULL DEFAULT 0,
+  `climb_reports` bigint unsigned NOT NULL DEFAULT 0,
+  `teleporthack_reports` bigint unsigned NOT NULL DEFAULT 0,
   PRIMARY KEY (`guid`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 DROP TABLE IF EXISTS `players_reports_status`;
 CREATE TABLE IF NOT EXISTS `players_reports_status` (
-  `guid` int(10) unsigned NOT NULL DEFAULT '0',
-  `creation_time` int(10) unsigned NOT NULL DEFAULT '0',
-  `average` float NOT NULL DEFAULT '0',
-  `total_reports` bigint(20) unsigned NOT NULL DEFAULT '0',
-  `speed_reports` bigint(20) unsigned NOT NULL DEFAULT '0',
-  `fly_reports` bigint(20) unsigned NOT NULL DEFAULT '0',
-  `jump_reports` bigint(20) unsigned NOT NULL DEFAULT '0',
-  `waterwalk_reports` bigint(20) unsigned NOT NULL DEFAULT '0',
-  `teleportplane_reports` bigint(20) unsigned NOT NULL DEFAULT '0',
-  `climb_reports` bigint(20) unsigned NOT NULL DEFAULT '0',
-  `teleporthack_reports` bigint(20) unsigned NOT NULL DEFAULT '0',
+  `guid` int unsigned NOT NULL DEFAULT 0,
+  `creation_time` int unsigned NOT NULL DEFAULT 0,
+  `average` float NOT NULL DEFAULT 0,
+  `total_reports` bigint unsigned NOT NULL DEFAULT 0,
+  `speed_reports` bigint unsigned NOT NULL DEFAULT 0,
+  `fly_reports` bigint unsigned NOT NULL DEFAULT 0,
+  `jump_reports` bigint unsigned NOT NULL DEFAULT 0,
+  `waterwalk_reports` bigint unsigned NOT NULL DEFAULT 0,
+  `teleportplane_reports` bigint unsigned NOT NULL DEFAULT 0,
+  `climb_reports` bigint unsigned NOT NULL DEFAULT 0,
+  `teleporthack_reports` bigint unsigned NOT NULL DEFAULT 0,
   PRIMARY KEY (`guid`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
