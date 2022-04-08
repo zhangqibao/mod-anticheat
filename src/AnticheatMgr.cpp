2861f1d686ad5759d74b1d7c65cb22bcde326302
MDIC <joshua.lee.betts@gmail.com>
2022-04-08 11:48:33 -0400

    Revert "Update: False Hit Reduction."
    This reverts commit 3abb14713c15ec9956dfa1b380938addc61ecbcd.

--- src/AnticheatMgr.cpp
+++ src/AnticheatMgr.cpp
@@ -478,17 +478,12 @@ void AnticheatMgr::SpeedHackDetection(Player* player, MovementInfo movementInfo)
     // We check the last MovementInfo for the falling flag since falling down a hill and sliding a bit triggered a false positive
     if ((clientSpeedRate > speedRate * 1.25f) && !m_Players[key].GetLastMovementInfo().HasMovementFlag(MOVEMENTFLAG_FALLING))
     {
-        if (!player->CanTeleport())
+        if (sConfigMgr->GetOption<bool>("Anticheat.WriteLog", true))
         {
-            if (sConfigMgr->GetOption<bool>("Anticheat.WriteLog", true))
-            {
-                LOG_INFO("module", "AnticheatMgr:: Speed-Hack detected player {} ({})", player->GetName(), player->GetGUID().ToString());
-            }
-
-            BuildReport(player, SPEED_HACK_REPORT);
+            LOG_INFO("module", "AnticheatMgr:: Speed-Hack detected player {} ({})", player->GetName(), player->GetGUID().ToString());
         }
-        else if (player->CanTeleport())
-            player->SetCanTeleport(false);
+
+        BuildReport(player, SPEED_HACK_REPORT);
     }
 }
 
