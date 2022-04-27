213973466262b74c98e8aa884cca5c08950c5c4b
MDIC <joshua.lee.betts@gmail.com>
2022-04-27 17:25:07 -0400

    fix (module\detection): Further Z-Axis rework

--- src/AnticheatMgr.cpp
+++ src/AnticheatMgr.cpp
@@ -241,7 +241,7 @@ void AnticheatMgr::ZAxisHackDetection(Player* player, MovementInfo movementInfo)
 
     // We want to exclude this LiquidStatus from detection because it leads to false positives on boats, docks etc.
     // Basically everytime you stand on a game object in water
-    if (player->GetLiquidData().Status == LIQUID_MAP_ABOVE_WATER)
+    if (player->GetLiquidData().Status == LIQUID_MAP_ABOVE_WATER && movementInfo.HasMovementFlag(MOVEMENTFLAG_ONTRANSPORT))
         return;
 
     // This is Black Magic. Check only for x and y difference but no z difference that is greater then or equal to z +2.5 of the ground
