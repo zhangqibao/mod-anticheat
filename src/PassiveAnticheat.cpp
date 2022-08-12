b8253027c8609f7af75e003651dd851b50d6fddf
Skjalf <47818697+Nyeriah@users.noreply.github.com>
2022-08-12 11:21:35 -0300

    feat: Implement database updater support

--- src/PassiveAnticheat.cpp
+++ src/PassiveAnticheat.cpp
@@ -24,9 +24,11 @@
 
 void startAnticheatScripts();
 void AddSC_anticheat_commandscript();
+void AddAntiCheatDBLoadingScripts();
 
 void Addmod_anticheatScripts()
 {
     startAnticheatScripts();
     AddSC_anticheat_commandscript();
+    AddAntiCheatDBLoadingScripts();
 }
